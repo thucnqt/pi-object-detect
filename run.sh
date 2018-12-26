6675cb2a4fc4105571a9123e31f810365295f7c4
Thucnqt <thucnqt@gmail.com>
2018-12-26 14:36:31 +0700

    first commit

---
+++ run.sh
@@ -0,0 +1,2 @@
+python pi_face_recognition.py --cascade haarcascade_frontalface_default.xml \
+	--encodings encodings.pickle
---
+++ train.sh
@@ -0,0 +1,2 @@
+python encode_faces.py --dataset dataset --encodings encodings.pickle \
+	--detection-method hog
